7d178f95fae83a93b3e6ef1b2876c0f5a96618e2
Link4Electronics <link4electronics@gmail.com>
2026-03-31 16:39:38 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,6 +8,7 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     nodejs \
+    npm    \
     yarn
 
 echo "Installing debloated packages..."
