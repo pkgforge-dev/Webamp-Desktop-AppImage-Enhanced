79839e35b74aaad444ad61350bab496e928d8a6d
Link4Electronics <link4electronics@gmail.com>
2026-03-31 17:52:12 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,14 +3,12 @@
 set -eu
 
 ARCH=$(uname -m)
-#VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
 VERSION=0.3.0
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/durasj/webamp-desktop/f165b34fa3f8a8b9c16f1153af469c191fdb3b89/res/icon.svg
-export DESKTOP=DUMMY
 export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
