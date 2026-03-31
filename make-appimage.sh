130a9338d5420e26f760e86a4b6691d9486f3149
Link4Electronics <link4electronics@gmail.com>
2026-03-31 16:08:16 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,9 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://raw.githubusercontent.com/durasj/webamp-desktop/f165b34fa3f8a8b9c16f1153af469c191fdb3b89/res/icon.svg
+export DESKTOP=DUMMY
+export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
 quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
