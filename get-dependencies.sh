1a76a462727cd5c40003bd99246dfed2b911119d
Link4Electronics <link4electronics@gmail.com>
2026-03-31 18:17:27 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,6 +28,14 @@ git clone https://github.com/durasj/webamp-desktop.git
 
 mkdir -p ./AppDir/bin
 cd webamp-desktop
+
+sed -i \
+  -e 's|const { width, height } = screen.getPrimaryDisplay().workAreaSize;|// & \n  const width = 1200;\n  const height = 800;|' \
+  -e 's/resizable: false/resizable: true/g' \
+  -e 's/movable: false/movable: true/g' \
+  -e 's/setIgnoreMouseEvents(true, { forward: true })/setIgnoreMouseEvents(false, { forward: false })/g' \
+  main.js
+
 yarn install
 npx electron-builder -l --x64
 mv -v artifacts/linux-unpacked/* ../AppDir/bin
