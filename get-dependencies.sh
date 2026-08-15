b0960210e7333f22147df867b39de1f4a0d710e6
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:37:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,12 +18,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
 # If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
 echo "Getting app..."
 echo "---------------------------------------------------------------"
 git clone https://github.com/durasj/webamp-desktop.git
