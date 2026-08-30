7267e08b0c59e0f215665d71ee4b960a98bbc2ba
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:33:21 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,15 +11,9 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=https://raw.githubusercontent.com/durasj/webamp-desktop/refs/heads/master/res/icon.png
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/webamp-desktop ./AppDir/bin/libffmpeg.so
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
